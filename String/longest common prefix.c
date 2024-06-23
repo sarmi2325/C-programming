96104bc59cb6c32a00f1a48948f55d2fb4151ae1
sarmi2325 <156922837+sarmi2325@users.noreply.github.com>
2024-06-23 16:06:25 +0530

    Create longest common prefix.c

---
+++ String/longest common prefix.c
@@ -0,0 +1,45 @@
+CODE:
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char* fn(char **str,int n){
+    if(n<=0){
+        return "";
+    }
+    char *prefix=(char*)malloc(strlen(str[0])+1*sizeof(char));
+    strcpy(prefix,str[0]);
+    for(int i=1;i<n;i++){
+        char *temp=(char*)malloc(strlen(prefix)+1*sizeof(char));
+        int j=0;
+        while(prefix[j]&&str[i][j]&&prefix[j]==str[i][j]){
+            temp[j]=prefix[j];
+            j++;
+        }
+        temp[j]='\0';
+    
+    strcpy(prefix,temp);
+    free(temp);
+    }
+    return prefix;
+    
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    char **str=(char**)malloc(n*sizeof(char*));
+    getchar();
+    for(int i=0;i<n;i++){
+        str[i]=(char*)malloc(100*sizeof(char));
+        fgets(str[i],100,stdin);
+        str[i][strcspn(str[i], "\n")] = '\0';
+    }
+    char *result=fn(str,n);
+    printf("%s",result);
+    for(int i=0;i<n;i++){
+        free(str[i]);
+    }
+    free(str);
+    free(result);
+    return 0;
+}
